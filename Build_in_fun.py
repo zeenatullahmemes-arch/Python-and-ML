1f7c13bcd14e956e497564b61294673d78f6f797
zeenatullahmemes-arch <zeenatullahmemes@gmail.com>
2025-08-28 11:34:34 -0700

    Add files via upload

---
+++ Build_in_fun.py
@@ -0,0 +1,40 @@
+# numbers=[1,2,4,3]
+# print(sorted(numbers))
+# print(max(numbers))
+# print(min(numbers))
+# print(bool(0))
+print(bool("Zeenat Ullah"))
+# print(bool(1))
+# print(bool(""))
+# print(bool("Zeenat Ullah"))
+# print(bool.__doc__) 
+print(len.__doc__) # used to give a information about any function
+
+# # ordinary function is used to print the asci code of  a character
+# # name="A"
+# # print("here is the asci code ",ord(name))
+# char=input("Enter any character for printing asci code")
+# print("the asci code of the B is",ord(char))
+
+# # chr function gives the character in return of ascii code number
+# num=int(input("enter any character for asci code"))
+# print("here is the character of asci code ",chr(num))
+
+# # all function checks all the conditions if true then return true otherwise false
+# # any function checks all the conditions if one is true `` then return true 
+# my_list=[True,True,True]
+# friends=[True,False,True]
+# print(all(my_list)) 
+# print(any(friends))
+
+price=89.2343
+# print(round(price,2))
+print(round(price,2))
+
+
+
+
+
+
+
+
